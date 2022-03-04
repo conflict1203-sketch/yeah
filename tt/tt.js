ea19f728c4edeb30570b0e1d1f02a6a8142f65d7
huhu shi <conflict1203@gmail.com>
2022-03-04 02:54:09 +0000

    Update tt/tt.js

--- tt/tt.js
+++ tt/tt.js
@@ -1,4 +1,5 @@
-re('\"pro_expired_at\":\".{20}','"pro_expired_at" : "2022-12-27 23:59:59"')
+re('\"pro_expired_at\":\".{20}','\"pro_expired_at\":\"2022-12-27 23:59:59\"')
+
 
 function re() {
     var body = $response.body;
