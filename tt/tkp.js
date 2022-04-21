e2f0fe4ab610e50dd024d8a213105e20cc44470b
huhu shi <conflict1203@gmail.com>
2022-04-21 15:40:49 +0000

    Update test.conf, tt/tkp.js

--- tt/tkp.js
+++ tt/tkp.js
@@ -2,6 +2,7 @@
 //re('\"ableToJoin\":false@\"joined\":false','\"ableToJoin\":true@\"joined\":true')
 
 re('false@\"districtForbidden\":true','true@\"districtForbidden\":false')
+re('\"errorCode\":403016','\"errorCode\":0')
 
 
 function re() {
