6c1c04ecfae82561fa9ae5f6f07acad86fcaa4b4
huhu shi <conflict1203@gmail.com>
2022-05-01 16:35:18 +0000

    Update tt/tcmm.js

--- tt/tcmm.js
+++ tt/tcmm.js
@@ -1,7 +1,7 @@
 
 
 
-re('false@\"vip_type\":1@"vip_overdue_time\":0@\"status\":\"normal\"@\"errCode\":52000','true@\"vip_type\":5@\"vip_overdue_time\":1951418898000@\"status\":\"vip\"@\"errCode\":0')
+re('false@\"vip_type\":1@"vip_overdue_time\":0@\"status\":\"normal\"@\"errCode\":52000@\"over_limit\":true@\"current\":\\d+@\"residence\":\\d+@\"max\":\\d+','true@\"vip_type\":5@\"vip_overdue_time\":1951418898000@\"status\":\"vip\"@\"errCode\":0@"over_limit\":false@\"current\":300000@\"residence\":700000@\"max\":1000000')
 
 function re() {
     var body = $response.body;
