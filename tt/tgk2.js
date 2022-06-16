29dc7680a9c15590612c89eea42f4c125562ed8f
huhu shi <conflict1203@gmail.com>
2022-06-16 02:29:09 +0000

    Update tt/tgk2.js, test.conf

---
+++ tt/tgk2.js
@@ -0,0 +1,24 @@
+
+
+
+re('"data".*"error"','"data":{"list":[{"type":"c","end_time":-1,"status":0},{"type":"d","end_time":1607183999,"status":0},{"type":"q","end_time":-1,"status":0},{"type":"p","end_time":1686882295,"status":1},{"type":"s","end_time":-1,"status":0}],"tag":{"has_first_auto_renewal_right":true,"auto_renewable":0,"experience_pvip":true},"pvip":{"is_paid":true,"current":{"pvip_type":5,"pvip_type_label":"体验卡"},"will":null}},"error"')
+
+
+
+
+function re() {
+    var body = $response.body;
+    if (arguments[0].includes("@")) {
+        var regs = arguments[0].split("@");
+        var strs = arguments[1].split("@");
+        for (i = 0;i < regs.length;i++) {
+            var reg = new RegExp(regs[i],"g");
+            body = body.replace(reg, strs[i]);
+        }
+    }
+    else {
+        var reg = new RegExp(arguments[0],"g");
+        body = body.replace(reg, arguments[1]);
+    }
+    $done(body);
+}
