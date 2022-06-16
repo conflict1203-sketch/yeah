6183b6eecc04b7a4f135c6fd8d8ecf7bfc94fd3c
huhu shi <conflict1203@gmail.com>
2022-06-16 02:52:19 +0000

    Update tt/tgk2.js

--- tt/tgk2.js
+++ tt/tgk2.js
@@ -1,7 +1,7 @@
 
 
 
-re('"data".*"error"','"data":{"list":[{"type":"c","end_time":-1,"status":0},{"type":"d","end_time":1607183999,"status":0},{"type":"q","end_time":-1,"status":0},{"type":"p","end_time":1686882295,"status":1},{"type":"s","end_time":-1,"status":0}],"tag":{"has_first_auto_renewal_right":true,"auto_renewable":0,"experience_pvip":true},"pvip":{"is_paid":true,"current":{"pvip_type":5,"pvip_type_label":"体验卡"},"will":null}},"error"')
+re('".*"error"','"data":{"list":[{"code":0,"type":"c","end_time":-1,"status":0},{"type":"d","end_time":1607183999,"status":0},{"type":"q","end_time":-1,"status":0},{"type":"p","end_time":1686882295,"status":1},{"type":"s","end_time":-1,"status":0}],"tag":{"has_first_auto_renewal_right":true,"auto_renewable":0,"experience_pvip":true},"pvip":{"is_paid":true,"current":{"pvip_type":5,"pvip_type_label":"体验卡"},"will":null}},"error"')
 
 
 
