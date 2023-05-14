e7c9ff4df70ba2de5f51ea60d8ca7b5e7135db76
huhu shi <conflict1203@gmail.com>
2023-05-14 13:19:17 +0000

    Update 2 files
    - /tt/testhx.js
    - /tt/ghtesthx.js

---
+++ tt/ghtesthx.js
@@ -0,0 +1,41 @@
+// getcode()
+re('\"sysScheduleId\":\"(.*?)\"@\"imageId\":\"(.*?)\"@\"verifyCode\":\"(.*?)\"','"sysScheduleId":"544630310997102592"@"imageId":"mtvlrr001qe2d6y48xselirjrlqy4dzq"@"verifyCode":"7906"')
+
+// "sysScheduleId":"557914079650824192"
+function re() {
+    var body = $response.body;
+    
+    console.log('before: ' + body)
+    if (arguments[0].includes("@")) {
+        var regs = arguments[0].split("@");
+        var strs = arguments[1].split("@");
+        for (i = 0;i < regs.length;i++) {
+            var reg = new RegExp(regs[i],"g");
+            body = body.replace(reg, strs[i]);
+        }
+    }
+    else {
+        var reg = new RegExp(arguments[0],"g");
+        body = body.replace(reg, arguments[1]);
+    }
+    console.log('after: ' + body)
+
+    $done(body);
+}
+
+
+// {
+//     "hospitalCode" : "HID0101",
+//     "sysTimeArrangeId" : "",
+//     "sysScheduleId" : "542336221569994752",
+//     "verifyCode" : "7906",
+//     "type" : "APP",
+//     "channelCode" : "PATIENT_IOS",
+//     "appCode" : "HXGYAPP",
+//     "hospitalAreaCode" : "HID0103",
+//     "imageId" : "mtvlrr001qe2d6y48xselirjrlqy4dzq",
+//     "cardId" : "262997045965623296",
+//     "encrypt" : "RAGLa0fLUMmr85VZpieplIaubNGbuGC1Ws/91Igo72o=",
+//     "appointmentType" : "1"
+//   }
+  
--- tt/testhx.js
+++ tt/testhx.js
@@ -1,46 +1,25 @@
 // getcode()
-// re('\"sysScheduleId\":\"(.*?)\"','"sysScheduleId":"542336221569994752"')
+re('\"sysScheduleId\":\"(.*?)\"','"sysScheduleId":"544630310997102592"')
 
-// // "sysScheduleId":"557914079650824192"
-// function re() {
-//     var body = $response.body;
+// "sysScheduleId":"557914079650824192"
+function re() {
+    var body = $response.body;
     
-//     console.log('before: ' + body)
-//     if (arguments[0].includes("@")) {
-//         var regs = arguments[0].split("@");
-//         var strs = arguments[1].split("@");
-//         for (i = 0;i < regs.length;i++) {
-//             var reg = new RegExp(regs[i],"g");
-//             body = body.replace(reg, strs[i]);
-//         }
-//     }
-//     else {
-//         var reg = new RegExp(arguments[0],"g");
-//         body = body.replace(reg, arguments[1]);
-//     }
-//     console.log('after: ' + body)
+    console.log('before: ' + body)
+    if (arguments[0].includes("@")) {
+        var regs = arguments[0].split("@");
+        var strs = arguments[1].split("@");
+        for (i = 0;i < regs.length;i++) {
+            var reg = new RegExp(regs[i],"g");
+            body = body.replace(reg, strs[i]);
+        }
+    }
+    else {
+        var reg = new RegExp(arguments[0],"g");
+        body = body.replace(reg, arguments[1]);
+    }
+    console.log('after: ' + body)
 
-//     $done(body);
-// }
+    $done(body);
+}
 
-
-const url = `https://sspai.com/api/v1/combo/recommend/page/get?limit=2&offset=0&t=1684052583584`;
-const method = `GET`;
-const headers = {
-'Connection' : `keep-alive`
-};
-const body = `{}`;
-
-const myRequest = {
-    url: url,
-    method: method,
-    headers: headers,
-    body: body
-};
-
-$task.fetch(myRequest).then(response => {
-    console.log(response.statusCode + "\n\n" + response.body);
-}, reason => {
-    console.log(reason.error);
-});
-$done($response.body);
