9ba5ee0f1534bf8e73c5571ee44085f3e258261a
huhu shi <conflict1203@gmail.com>
2023-06-14 14:45:56 +0000

    Update file tdd9.js

--- tt/tdd9.js
+++ tt/tdd9.js
@@ -1,5 +1,33 @@
 var modifiedHeaders = $request.headers;
 
-modifiedHeaders['Cookie'] = 'ISID=e29833cd380e47dabfa5143461aea0d5; acw_tc=2f6fc13016867509087403403e1f29ea5f2349af53ae5c825626e57c9d0688; aliyungf_tc=0f881a347bde9c0d887e6e8a5e0581442488466d2c583f0dbdfb5830a3570da0';
+modifiedHeaders['Cookie'] = 'acw_tc=784c10e816867533583422662e3eb197995511b06b3a1455debf3b5caccd74; ISID=7bba5fe019f8f92dd64e8867758bb8bf';
+
+
+modifiedHeaders['Xi-SSID'] = 'c414a080-f847-42f7-b2ea-06b31a586fc9';
+
+
+modifiedHeaders['G-Auth-Appid'] = '5a27d122ee4638163f594393';
+
+
+modifiedHeaders['Xi-UID'] = '222842815';
+
+
+modifiedHeaders['Xi-DV'] = 'iPhone11,8';
+
+
+modifiedHeaders['G-Auth-Ts'] = '1686753364';
+
+
+modifiedHeaders['G-Auth-Sign'] = '4e8671988d9ed0fe54adee9a0ea9ce4944036059';
+
+
+modifiedHeaders['G-Auth-Nonce'] = '5aa4566e45c22b3327403f3a3c3bb3f0';
+
+modifiedHeaders['G-Auth-Token'] = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJpZ2V0Z2V0LmNvbSIsImV4cCI6MTY4OTM0NTM1MiwiaWF0IjoxNjg2NzUzMzUyLCJpc3MiOiJEREdXIEpXVCBNSURETEVXQVJFIiwibmJmIjoxNjg2NzUzMzUyLCJzdWIiOiIyMjI4NDI4MTUiLCJkZXZpY2VfaWQiOiIxYThjNDBlYmNiMjc0MmI5YWRiNGQ0NTc5MzNjNjk0YyIsImRldmljZV90eXBlIjoiIn0.hJYPCKp1TBqPBO-OUz9G25ojoO0Bb7ajgFWR1HIfTOsqeUINYrXAE6aTVrAsRkKFsKd6H4UoxRnNF-Ed4zN8mg';
+modifiedHeaders['Xi-S-D'] = '6752fce8f23c4d0aa5281c0a58991422';
+modifiedHeaders['Po-Idfa'] = '43973EB5-9F7A-4986-8D25-9B6B928E74D1';
+modifiedHeaders['Cookie'] = '';
+
+
 
 $done({headers : modifiedHeaders});
