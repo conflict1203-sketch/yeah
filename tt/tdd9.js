2f79af1de74d159817ac410f4633a0a4c94413db
huhu shi <conflict1203@gmail.com>
2023-06-15 14:59:50 +0000

    Update file tdd9.js

--- tt/tdd9.js
+++ tt/tdd9.js
@@ -1,9 +1,9 @@
 var modifiedHeaders = $request.headers;
 
-modifiedHeaders['Cookie'] = 'acw_tc=784c10e816867533583422662e3eb197995511b06b3a1455debf3b5caccd74; ISID=7bba5fe019f8f92dd64e8867758bb8bf';
+modifiedHeaders['Cookie'] = 'acw_tc=0a6fd19616868388513443166ea737e41ec388219a837101d4efc75d09434c; aliyungf_tc=d97d855d8ab084deab8cb9251cfa6a1a16a222d3c226f01c60617a4e798c006d; ISID=7bba5fe019f8f92dd64e8867758bb8bf';
 
 
-modifiedHeaders['Xi-SSID'] = 'c414a080-f847-42f7-b2ea-06b31a586fc9';
+modifiedHeaders['User-Agent'] = 'LuoJiFMIOS/10.14.0 (iPhone; iOS 16.0.3; Scale/2.00)';
 
 
 modifiedHeaders['G-Auth-Appid'] = '5a27d122ee4638163f594393';
@@ -15,6 +15,25 @@ modifiedHeaders['Xi-UID'] = '222842815';
 modifiedHeaders['Xi-DV'] = 'iPhone11,8';
 
 
+
+
+modifiedHeaders['Xi-D'] = '1a8c40ebcb2742b9adb4d457933c694c';
+
+modifiedHeaders['Xi-OV'] = '16.0.3';
+
+
+modifiedHeaders['Xi-SSID'] = 'c414a080-f847-42f7-b2ea-06b31a586fc9';
+
+
+modifiedHeaders['Po-Ov'] = '16.0.3';
+
+
+modifiedHeaders['Xi-SCALE'] = '2';
+
+modifiedHeaders['Xi-SCR'] = '828*1792';
+
+
+
 // modifiedHeaders['G-Auth-Ts'] = '1686753364';
 
 
@@ -23,7 +42,7 @@ modifiedHeaders['Xi-DV'] = 'iPhone11,8';
 
 // modifiedHeaders['G-Auth-Nonce'] = '5aa4566e45c22b3327403f3a3c3bb3f0';
 
-modifiedHeaders['G-Auth-Token'] = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJpZ2V0Z2V0LmNvbSIsImV4cCI6MTY4OTM0NTM1MiwiaWF0IjoxNjg2NzUzMzUyLCJpc3MiOiJEREdXIEpXVCBNSURETEVXQVJFIiwibmJmIjoxNjg2NzUzMzUyLCJzdWIiOiIyMjI4NDI4MTUiLCJkZXZpY2VfaWQiOiIxYThjNDBlYmNiMjc0MmI5YWRiNGQ0NTc5MzNjNjk0YyIsImRldmljZV90eXBlIjoiIn0.hJYPCKp1TBqPBO-OUz9G25ojoO0Bb7ajgFWR1HIfTOsqeUINYrXAE6aTVrAsRkKFsKd6H4UoxRnNF-Ed4zN8mg';
+modifiedHeaders['G-Auth-Token'] = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJpZ2V0Z2V0LmNvbSIsImV4cCI6MTY4OTQzMDg1MSwiaWF0IjoxNjg2ODM4ODUxLCJpc3MiOiJEREdXIEpXVCBNSURETEVXQVJFIiwibmJmIjoxNjg2ODM4ODUxLCJzdWIiOiIyMjI4NDI4MTUiLCJkZXZpY2VfaWQiOiIxYThjNDBlYmNiMjc0MmI5YWRiNGQ0NTc5MzNjNjk0YyIsImRldmljZV90eXBlIjoiIn0.5c4q-CVcQk1gAUf6VHfmFyBSWzfqWROAXebDoqdWrnzkWUh65nzStdZzaDKQ5Dqub-7KcpDXTUlBb96ozmWY3g';
 modifiedHeaders['Xi-S-D'] = '6752fce8f23c4d0aa5281c0a58991422';
 modifiedHeaders['Po-Idfa'] = '43973EB5-9F7A-4986-8D25-9B6B928E74D1';
 
