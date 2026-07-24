899b94462784cee9b5aa7723e2d0359a4e01e94b
huhu.shi <huhu.shi@hand-china.com>
2026-07-24 17:33:30 +0800

    [ADD] 12

--- tt/mr.js
+++ tt/mr.js
@@ -95,7 +95,9 @@ let rules = [
 
 // 先用配置查找是否匹配某个规则
 const matched = rules.find(rule =>
-    rule.domains.some(domain => url.includes(domain))
+    $.log('匹配规则:', rule.name);
+    $.log('匹配规则:',  url);
+    rule.domains.some(domain => url.includes(domain); $.log('匹配结果：' + domain => url.includes(domain)))
 );
 
 // 用 switch 来处理结果
