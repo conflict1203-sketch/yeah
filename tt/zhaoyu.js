be2a1f2ea3668b051004de8105e2e432d736b365
huhu.shi <huhu.shi@hand-china.com>
2026-08-05 21:41:26 +0800

    [ADD] 12

--- tt/zhaoyu.js
+++ tt/zhaoyu.js
@@ -6,17 +6,38 @@ function main() {
     var body = $response.body;
     
     // 将所有装备品质升级到最高级 5（先处理复杂结构）
-    var regWf = /"wf"\s*:\s*\[([\s\S]*?)\]/g;
-    body = body.replace(regWf, function(match, wfContent) {
-        var items = wfContent.match(/\[\s*\d+\s*,\s*\d+\s*\]/g);
-        if (!items) return match;
-        
-        var newItems = items.map(function(item) {
-            var arr = item.match(/\d+/g).map(Number);
-            return "[" + arr[0] + ",5]";
-        });
-        return '"wf": [' + newItems.join(', ') + ']';
-    });
+    // 找到 "wf" 键，然后手动匹配对应的数组
+    var wfIndex = body.indexOf('"wf"');
+    if (wfIndex !== -1) {
+        // 从 [ 开始位置
+        var arrayStart = body.indexOf('[', wfIndex);
+        if (arrayStart !== -1) {
+            // 使用深度计数器找到匹配的 ]
+            var depth = 0;
+            var i = arrayStart;
+            while (i < body.length) {
+                if (body[i] === '[') depth++;
+                else if (body[i] === ']') {
+                    depth--;
+                    if (depth === 0) break;
+                }
+                i++;
+            }
+            
+            var wfContent = body.substring(arrayStart + 1, i);  // 去掉外层 []
+            var items = wfContent.match(/\[\s*\d+\s*,\s*\d+\s*\]/g);
+            
+            if (items) {
+                var newItems = items.map(function(item) {
+                    var arr = item.match(/\d+/g).map(Number);
+                    return "[" + arr[0] + ",5]";
+                });
+                
+                var newArray = newItems.join(', ');
+                body = body.substring(0, arrayStart + 1) + newArray + body.substring(i);
+            }
+        }
+    }
     
     // 修改钻石数量
     var regGd = /"gd"\s*:\s*\d+/g;
@@ -28,7 +49,7 @@ function main() {
     
     // 修改金币数量
     var regCs = /"cs"\s*:\s*\d+/g;
-    body = body.replace(regCs, '"cs": 286');
+    body = body.replace(regCs, '"cs": 187');
     
     // 解锁所有头像（将 aul 数组全部改成 1）
     var regAul = /"aul"\s*:\s*\[[\s\S]*?\]/g;
