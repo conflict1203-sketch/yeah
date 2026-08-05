ea1fe1a4d8bae2d1134b08d51963f99ec50629b0
huhu.shi <huhu.shi@hand-china.com>
2026-08-05 21:19:59 +0800

    [ADD] 12

--- tt/zhaoyu.js
+++ tt/zhaoyu.js
@@ -1,20 +1,30 @@
 // 赵云与阿斗 - 修改登录信息脚本
-re('\"gd\":\\d+@\"sm\":\\d+@\"cs\":\\d+', '\"gd\":1686@\"sm\":30@\"cs\":2114')
 
+main();
 
-function re() {
+function main() {
     var body = $response.body;
-    if (arguments[0].includes("@")) {
-        var regs = arguments[0].split("@");
-        var strs = arguments[1].split("@");
-        for (i = 0; i < regs.length; i++) {
-            var reg = new RegExp(regs[i], "g");
-            body = body.replace(reg, strs[i]);
-        }
-    }
-    else {
-        var reg = new RegExp(arguments[0], "g");
-        body = body.replace(reg, arguments[1]);
-    }
+    
+    // 修改钻石数量
+    var reg1 = /"gd":\d+/g;
+    body = body.replace(reg1, '"gd":1686');
+    
+    // 修改体力值
+    var reg2 = /"sm":\d+/g;
+    body = body.replace(reg2, '"sm":30');
+    
+    // 修改金币数量
+    var reg3 = /"cs":\d+/g;
+    body = body.replace(reg3, '"cs":286');
+    
+    // 解锁所有头像（将 aul 数组全部改成 1）
+    var reg4 = /"aul":\[[^\]]+\]/g;
+    body = body.replace(reg4, '"aul":[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]');
+    
+    // 将所有装备品质升级到最高级 5
+    var reg5 = /"wf":\[[^\]]+\]/g;
+    body = body.replace(reg5, '"wf":[[2,5],[22,5],[32,5],[21,5],[23,5],[14,5],[3,5],[35,5],[13,5],[34,5],[26,5],[37,5],[16,5],[27,5],[28,5],[8,5],[19,5],[7,5],[30,5]]');
+    
     $done(body);
 }
+
