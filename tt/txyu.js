f5146eec2df6db2c257a096facc62192f4583a8f
huhu.shi <huhu.shi@hand-china.com>
2026-08-25 19:58:44 +0800

    [ADD] 12

---
+++ tt/txyu.js
@@ -0,0 +1,23 @@
+
+re('pending@\"error\":\\D+.','success@\"error\":0')
+// re('\"free\":false@\"error\":\\D+.','\"free\":true@\"error\":0')
+
+
+
+
+function re() {
+    var body = $response.body;
+    if (arguments[0].includes("@")) {
+        var regs = arguments[0].split("@");
+        var strs = arguments[1].split("@");
+        for (i = 0;i < regs.length;i++) {
+            var reg = new RegExp(regs[i],"g");
+            body = body.replace(reg, strs[i]);
+        }
+    }
+    else {
+        var reg = new RegExp(arguments[0],"g");
+        body = body.replace(reg, arguments[1]);
+    }
+    $done(body);
+}
